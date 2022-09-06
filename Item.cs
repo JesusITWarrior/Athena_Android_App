e897b2851fca1e2730955699cd53e01db40962e8
JesusWarrior <47158026+JesusITWarrior@users.noreply.github.com>
2022-09-06 08:48:23 -0500

    Working on List management

---
+++ Item.cs
@@ -0,0 +1,94 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IAPYX_INNOVATIONS_RETROFIT_FRIDGE_APP
+{
+    class Item
+    {
+        public event EventHandler ItemChanged;
+        public string itemName { get; set; }
+        public int itemAmount { get; set; }
+
+        public void eventInvocation()
+        {
+            ItemChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    class ItemListViewAdapter : BaseAdapter<Item> {
+        public List<Item> inventory;
+        private Context ctx;
+
+        public ItemListViewAdapter(Context ctx, List<Item> list)
+        {
+            inventory = list;
+            this.ctx = ctx;
+
+        }
+
+        public override int Count
+        {
+            get { return inventory.Count; }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override Item this[int position]
+        {
+            get { return inventory[position]; }
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView;
+            if(row == null)
+            {
+                row = LayoutInflater.From(ctx).Inflate(Resource.Layout.list_view, null, false);
+            }
+
+            TextView txtName = row.FindViewById<TextView>(Resource.Id.itemName);
+            Button sub = row.FindViewById<Button>(Resource.Id.subtractItemButton);
+            sub.Click += (o, e) =>
+            {
+                //get item here from list
+                if (inventory[position].itemAmount > 0)
+                {
+                    inventory[position].itemAmount--;
+                    inventory[position].eventInvocation();
+                }
+                if (inventory[position].itemAmount == 0)
+                {
+                    //Warn user that there's nothing left, and ask if they want to clear it.
+                    //if they say yes, clear item from list and visual list
+                    //else leave it be.
+                }
+            };
+            Button add = row.FindViewById<Button>(Resource.Id.addItemButton);
+            add.Click += (o, e) =>
+            {
+                //Should add items to this object
+                inventory[position].itemAmount++;
+                inventory[position].eventInvocation();
+            };
+
+            TextView txtAmount = row.FindViewById<TextView>(Resource.Id.itemAmount);
+
+            txtName.Text = inventory[position].itemName;
+            txtAmount.Text = inventory[position].itemAmount.ToString();
+
+            return row;
+        }
+    }
+
+}
--- MainActivity.cs
+++ MainActivity.cs
@@ -1,20 +1,43 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using Android.Widget;
+using System.Collections.Generic;
 
 namespace IAPYX_INNOVATIONS_RETROFIT_FRIDGE_APP
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        List<Item> inventory;
+        private ListView lv;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            ActionBar.Hide();
+            TextView title = FindViewById<TextView>(Resource.Id.ListTitle);
+            Button addButton = FindViewById<Button>(Resource.Id.addToList);
+            //addButton.Click += (e,o)=>
+            //{
+            //    StartActivity(new Android.Content.Intent(this, typeof(Test)));
+            //};
+            addButton.Click += AddNewItem;
+
+            lv = FindViewById<ListView>(Resource.Id.inventory);
+
+            inventory = new List<Item>();
+            inventory.Add(new Item() { itemAmount = 5, itemName = "Apple"});
+            inventory.Add(new Item() { itemAmount = 2, itemName = "Banana" });
+
+            ItemListViewAdapter adapter = new ItemListViewAdapter(this, inventory);
+            lv.Adapter = adapter;
+            
+            //title.PaintFlags = Android.Graphics.PaintFlags.UnderlineText;
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -22,5 +45,44 @@ namespace IAPYX_INNOVATIONS_RETROFIT_FRIDGE_APP
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        public void UpdateView(object o, EventArgs e)
+        {
+            ItemListViewAdapter adapter = new ItemListViewAdapter(this, inventory);
+            lv.Adapter = adapter;
+        }
+
+        public void AddNewItem(object o, EventArgs e)
+        {
+            string itemToAdd = FindViewById<TextView>(Resource.Id.inputField1).Text;
+            bool foundIt = false;
+            for(int i = 0; i < inventory.Count; i++)
+            {
+                if(inventory[i].itemName == itemToAdd)
+                {
+                    inventory[i].itemAmount++;
+                    foundIt = true;
+                    break;
+                }
+            }
+            if (!foundIt)
+            {
+                Item newItem = new Item();
+                newItem.itemName = itemToAdd;
+                newItem.itemAmount = 1;
+                newItem.ItemChanged += UpdateView;
+                inventory.Add(newItem);
+            }
+
+            ItemListViewAdapter adapter = new ItemListViewAdapter(this, inventory);
+            lv.Adapter = adapter;
+        }
+
+        //    public void RemoveItem(object o, EventArgs e)
+        //    {
+        //        GroceryListItem item = new GroceryListItem();
+        //        //Removes item from the list after the warning prompt
+        //        groceryList.Remove(item);
+        //    }
     }
 }
