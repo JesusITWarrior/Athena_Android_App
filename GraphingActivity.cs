f78345df55d2228d73b347d8b41c37ef6f77717c
JesusWarrior <47158026+JesusITWarrior@users.noreply.github.com>
2022-11-09 14:29:34 -0600

    Started implementing the WebView chart generation from Google Charts.

--- GraphingActivity.cs
+++ GraphingActivity.cs
@@ -9,13 +9,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using AndroidX.AppCompat.App;
+using Android.Webkit;
 
 namespace IAPYX_INNOVATIONS_RETROFIT_FRIDGE_APP
 {
     [Activity(Label = "GraphingActivity")]
     public class GraphingActivity : AppCompatActivity
     {
-        
+        WebView graphView;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -23,8 +24,12 @@ namespace IAPYX_INNOVATIONS_RETROFIT_FRIDGE_APP
 
             // Create your application here
             SetContentView(Resource.Layout.usage_graph);
-
-            
+            if (!DatabaseManager.isOnline)
+            {
+                //Don't load any graphs and warn user!
+                return;
+            }
+            graphView = new WebView(this);
         }
     }
 }
